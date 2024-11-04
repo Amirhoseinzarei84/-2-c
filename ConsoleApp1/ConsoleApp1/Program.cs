f9e0741bdad16cc61f1214a12a6f6dfab7c16fd6
amirhoseinzarei <amirhoseinzarei8484@gmail.com>
2024-11-04 23:34:30 +0330

    Add files via upload
    تمرین 2 اعداد اینه ای c

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Perfect numbers between {num1} and {num2}: ");
+
+        for (int i = num1; i <= num2; i++)
+        {
+            bool isPerfect = true;
+
+            for (int j = 1; j < i; j++)
+            {
+                if (i % j == 0 && i / j != j)
+                {
+                    isPerfect = false;
+                    break;
+                }
+            }
+
+            if (isPerfect)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
